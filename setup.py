9fd4708f1c1e6f2f85b2bb1b5cd744d09ebe2886
Max Bothe <mathebox@gmail.com>
2016-02-10 11:39:02 +0100

    Change to custom pyhdb version number.

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(
     url='https://github.com/mathebox/django_hana',
     packages=['django_hana'],
     install_requires = [
-        'pyhdb==0.3.0'
+        'pyhdb==0.3.1-mod'
     ],
     dependency_links = [
-        'https://github.com/mathebox/PyHDB/archive/171d75f1708b2297971a4df89217cd348f2b47b6.zip#egg=pyhdb-0.3.0'
+        'https://github.com/mathebox/PyHDB/archive/f3cb802a9da783238c937c0175a36c771f77a9d3.zip#egg=pyhdb-0.3.1-mod'
     ],
 )
